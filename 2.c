2ea5e51680aa4cb4b4c2dbe73ca9622bf012a3d8
EusElif <euselif9@gmail.com>
2026-06-20 11:45:45 -0300

    Finalizado 2

--- 2.c
+++ 2.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 
-void inverterVetor(int *v,int tam){
-    int aux;
-    for(int i=0;i<tam/2;i++){
-        aux=*(v+i);
-        *(v+i)=*(v+(tam-1)-i);
-        *(v+(tam-1)-i) = aux;
-    }
-}
 //procurar outro nome ou não
-void autobots(int *v,int tamK,int tamvet){  
-    
+void autobots(int *v,int tamK,int tamvet){ 
+    //inverter vetor
+    for(int i=0;i<tamvet/2;i++){
+        int aux = *(v+i);
+        *(v+i) = *(v+(tamvet-1)-i);
+        *(v+(tamvet-1)-i) = aux;
+    }
+   /*
+    printf("Vetor Invertido: ");
+    for(int i=0;i<tamvet;i++){
+        printf(" %d ", *(v+i));
+    }
+    printf("\n");
+    */
+    tamK %= tamvet;
+    for(int j=0;j<tamK;j++){
+        int ultimo = *(v+tamvet-1);
+        for (int i = tamvet -1; i>0; i--){
+            *(v+i) = *(v+i-1);
+        }
+        *v = ultimo;
+    } 
 }
 
 int main(){
     int n;
     printf("Tamanho do Vetor: ");
     scanf("%d",&n);
+    
     int v[n];
     printf("Preencha o Vetor.\n");
     for(int i=0;i<n;i++){
         printf("V[%d]: ",i+1);
         scanf("%d",&v[i]);
     }
+    
     int k;
     printf("Quantidades de vezes que irá rodar: ");
     scanf("%d",&k);
-    inverterVetor(v,n);
-    //retirar depois, apenas verificação
-    printf("Vetor Invertido: ");
-    for(int i=0;i<n;i++){
-        printf(" %d ",v[i]);
-    }
-    printf("\n");
-    //fim da verificação
-    //mexer pra la
+
     autobots(v,k,n);
     printf("Vetor Invertido e rodado %d vezes: ",k);
     for(int i=0;i<n;i++){
