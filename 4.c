6e6da80925c8ffe1428470f0f4446375c704ae89
EusElif <euselif9@gmail.com>
2026-06-21 19:12:14 -0300

    foi?

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n,t;
+    while(n>0 || ){
+
+    }
+    if(t == 0 && n == 0 ){
+        printf("Verificação\n");
+        return 0;
+    }else{
+
+    }
+}
