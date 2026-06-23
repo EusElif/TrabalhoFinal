f21eb3d65e2705bb320398c126815805832b6fe4
danielrm21 <danielmourarm21@gmail.com>
2026-06-22 22:05:28 -0300

    atualização questão 1

--- 1.c
+++ 1.c
@@ -6,42 +6,49 @@ void ordenaPonteiros(int **vPtr, int n)
 
     for (i = 0; i < n; i++)
     {
-        for ( j = i + 1; j < n; j++)
+        for (j = i + 1; j < n; j++)
         {
             if (**(vPtr + i) > **(vPtr + j))
             {
-            int *temp = *(vPtr + i);
-            *(vPtr + i) = *(vPtr + j);
-            *(vPtr + j) = temp;
+                int *temp = *(vPtr + i);
+                *(vPtr + i) = *(vPtr + j);
+                *(vPtr + j) = temp;
             }
-        
         }
-        
     }
-    
 }
 
 void imprimePonteiros(int **vPtr, int n)
 {
-    printf("Organizacao dos ponteiros: \n");
+    printf("Organizacao dos ponteiros:\n");
+
     for (int i = 0; i < n; i++)
     {
-        printf("vPtr[%d] -> %d ", i, **(vPtr + i));
+        printf("vPtr[%d] -> %d\n", i, **(vPtr + i));
     }
 }
 
 int main()
 {
-    int V[5] = {50, 10, 30, 20, 40};
-    int *vPtr[5];
+    int n;
 
-    for (int i = 0; i < 5; i++)
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n);
+
+    int V[n];
+    int *vPtr[n];
+
+    printf("Digite os elementos do vetor:\n");
+    for (int i = 0; i < n; i++)
     {
+        printf("V[%d] = ", i);
+        scanf("%d", &V[i]);
+
         vPtr[i] = &V[i];
     }
-    
-    ordenaPonteiros(vPtr, 5);
-    imprimePonteiros(vPtr, 5);
+
+    ordenaPonteiros(vPtr, n);
+    imprimePonteiros(vPtr, n);
 
     return 0;
 }
