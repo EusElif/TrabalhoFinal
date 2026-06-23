daeb82cdca9411adcb802a38c5cea9bc2aaf325b
EusElif <euselif9@gmail.com>
2026-06-22 21:33:10 -0300

    Foi?

--- 5.c
+++ 5.c
@@ -5,21 +5,14 @@ void tamanhoCollatz(int col){
     int Tamanho_maior_cadeia=1;
     int cont_cadeia;
 
+    FILE *arquivo = fopen("todascadeias.txt","w");
+    fprintf(arquivo, "Sequencia com o numero até %d\n",col);
     for(int i=1;i<= col;i++){
         int num = i;
-        printf("%d: ",num);
-        if(num == 1){
-            printf("1");
-        }
+        fprintf(arquivo, "%d: %d",i ,num);
         
-        if(col == 1){
-            Num_maior_cadeia = 1;
-            Tamanho_maior_cadeia = 1;
-        }
-
-        printf("%d", num);
         if(num > 1){
-            printf(" -> ");
+            fprintf(arquivo," -> ");
         }
 
         cont_cadeia=1;
@@ -30,9 +23,9 @@ void tamanhoCollatz(int col){
             }else{
                 num=3*num+1;
             }
-            printf("%d", num);
+            fprintf(arquivo, "%d", num);
             if(num>1){
-                printf(" -> ");
+                fprintf(arquivo, " -> ");
             }
             cont_cadeia++;
         }
@@ -41,9 +34,11 @@ void tamanhoCollatz(int col){
             Tamanho_maior_cadeia = cont_cadeia;
             Num_maior_cadeia = i;
         }
-        printf("\n");
+        fprintf(arquivo,"\n");
     }
     
+    fclose(arquivo);
+
     printf("Num_maior_cadeia = %d\n", Num_maior_cadeia);
     printf("Tamanho_maior_cadeia = %d\n", Tamanho_maior_cadeia);
 }
@@ -57,6 +52,5 @@ int main(){
             printf("Numero maior que 1.000.000, por favor, solicite um numero menor.\n");
         }
     } while (n>1000000);
-    printf("Sequencia com o numero até %d\n",n);
     tamanhoCollatz(n);
 }
